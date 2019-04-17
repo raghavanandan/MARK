62c663dbcf30eae257a2119ba18b2ad9e338ecec
Kshitij Sood <kshitijsood@Kshitijs-MacBook-Pro.local>
2019-04-16 22:51:32 -0700

    Model prep and selection

--- MarkBackend/src/main/java/com/mark/controller/ApiController.java
+++ MarkBackend/src/main/java/com/mark/controller/ApiController.java
@@ -2,10 +2,24 @@ package com.mark.controller;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.ml.Pipeline;
+import org.apache.spark.ml.PipelineModel;
+import org.apache.spark.ml.PipelineStage;
+import org.apache.spark.ml.Transformer;
+import org.apache.spark.ml.classification.LogisticRegression;
+import org.apache.spark.ml.classification.LogisticRegressionModel;
+import org.apache.spark.ml.classification.NaiveBayes;
+import org.apache.spark.ml.classification.NaiveBayesModel;
+import org.apache.spark.ml.feature.IndexToString;
+import org.apache.spark.ml.feature.OneHotEncoder;
+import org.apache.spark.ml.feature.StringIndexer;
+import org.apache.spark.ml.feature.StringIndexerModel;
+import org.apache.spark.ml.feature.VectorAssembler;
+import org.apache.spark.mllib.evaluation.MulticlassMetrics;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
@@ -24,6 +38,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.mark.pojo.ModelSelection;
 import com.mark.pojo.Response;
 import com.mark.pojo.StringParser;
 import com.mark.storage.Mongo;
@@ -57,6 +72,12 @@ public class ApiController {
 	private static Dataset<Row> masterDf;
 
 	private static Dataset<Row> currentDf;
+	
+	private static PipelineModel pipelineModel;
+
+	private static Dataset<Row> training;
+
+	private static Dataset<Row> testing;
 
 	@RequestMapping("wordcount")
 	public ResponseEntity<List<Count>> words() {
@@ -115,15 +136,15 @@ public class ApiController {
 			currentDf = masterDf;
 		}
 
-		
+
 		Dataset<Row> frameDf = null;
-		
+
 		switch (frame) {
 		case "master":
 			frameDf = masterDf;
 
 			break;
-		
+
 		case "current":
 			frameDf = currentDf;
 
@@ -302,38 +323,159 @@ public class ApiController {
 		return new ResponseEntity<>(js, HttpStatus.OK);
 
 	}
-	
+
 	@RequestMapping(value="statistics")
 	public ResponseEntity<JSONObject> statistics(@RequestParam("columns") List<String> columns){
-		
+
 		if (currentDf == null) {
 			currentDf = masterDf;
 		}
-		
+
 		String[] cols = new String[columns.size()];
 		cols = columns.toArray(cols);
+
+		Dataset<Row> stats = currentDf.describe();
 		
-		Dataset<Row> col1 = currentDf.describe().select(currentDf.col(columns.get(0)));
+		Dataset<Row> col1 = stats.select(stats.col("summary"), stats.col(columns.get(0)));
 		
+
 		if (columns.size()>1) {
-			col1 = currentDf.describe().select(currentDf.col(columns.get(0)),currentDf.col(columns.get(1)));
+			col1 = stats.select(stats.col("summary"), stats.col(columns.get(0)),stats.col(columns.get(1)));
 		}
-		
+
 		JSONObject js = Utils.convertFrameToJson2(col1.collectAsList());
-		
+
 		Tuple2<String, String>[] dtypes = col1.dtypes();
 
 		JSONArray header = Utils.getTypes(dtypes);
 
 		js.put("header", header);
 		return new ResponseEntity<>(js, HttpStatus.OK);
-		
-		
-		
+
 	}
-	
-	
-	
+
+	private static void resetModel() {
+		training = null;
+		testing = null;
+		pipelineModel = null;
+	}
+
+
+	@RequestMapping(value="prepare-model",  method = RequestMethod.POST, produces =MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Response> prepareModel(@RequestBody ModelSelection modelSelection){
+
+		System.out.println("ModelData "+modelSelection);
+
+		resetModel();
+
+		StringIndexer tr = new StringIndexer().setInputCol(modelSelection.getOutputCol()).setOutputCol("label");
+		
+		ArrayList<PipelineStage> stages = new ArrayList<>();
+
+		stages.add(tr);
+
+		List<String> categorical = new ArrayList<>();
+
+		List<String> numerical = new ArrayList<>();
+
+
+		if (currentDf == null) {
+			currentDf = masterDf;
+		}
+
+
+		for(Tuple2<String, String> tup: currentDf.dtypes()){
+			if(tup._1 == "label")
+
+				continue;
+
+			if(tup._2 == "StringType") {
+				categorical.add(tup._1);
+			}
+			else {
+				numerical.add(tup._1);
+			}
+
+		}
+
+
+
+		for(String str: categorical){
+
+			StringIndexer strIndexer = new StringIndexer().setInputCol(str).setOutputCol(str + "Index");
+			OneHotEncoder encoder = new OneHotEncoder().setInputCol(strIndexer.getOutputCol()).setOutputCol(str + "classVec");
+
+			stages.add(strIndexer);
+			stages.add(encoder);
+		}
+
+		List<String> assemblerInputs = new ArrayList<>(numerical);
+		for(String str: categorical) {
+			assemblerInputs.add(str + "classVec");
+		}
+
+		VectorAssembler assembler = new VectorAssembler().setInputCols(assemblerInputs.toArray(new String[0])).setOutputCol("features");
+		stages.add(assembler);
+
+		Pipeline partialPipeline = new Pipeline().setStages(stages.toArray(new PipelineStage[0]));
+
+		pipelineModel = partialPipeline.fit(currentDf);
+
+		Dataset<Row> preppedDataDF = pipelineModel.transform(currentDf);
+
+		System.out.println(modelSelection.getTestSplit()/100.0);
+
+		System.out.println(modelSelection.getTrainSplit()/100.0);
+
+		Dataset<Row>[] ds = preppedDataDF.randomSplit(new double[] {modelSelection.getTrainSplit()/100.0, modelSelection.getTestSplit()/100.0});
+
+		training = ds[0];
+		testing = ds[1];
+
+		preppedDataDF.show();
+
+		return null;
+
+	}
+
+
+	@RequestMapping(value="predict")
+	public ResponseEntity<Response> predict(){
+
+		//LogisticRegressionModel lrModel = new LogisticRegression().fit(training);
+		
+		NaiveBayesModel lrModel = new NaiveBayes().fit(training);
+		
+		Dataset<Row> predictions = lrModel.transform(testing);
+		predictions.show();
+
+		Transformer[] _stages = pipelineModel.stages();
+
+		StringIndexerModel temp = (StringIndexerModel)_stages[0];
+
+		
+		IndexToString trs = new IndexToString().setLabels(temp.labels()).setInputCol("prediction").setOutputCol("prediction-original");
+		predictions = trs.transform(predictions);
+		
+		predictions.show();
+
+		
+//		RDD<Row> temp_rdd = testing.select("sexIndex", "prediction").map(new Function1<Row, Tuple2>(){});
+		
+		
+		
+		
+		
+		MulticlassMetrics metrics = new MulticlassMetrics(predictions.select("SexIndex", "prediction"));
+		
+		System.out.println(metrics.accuracy());
+		System.out.println(metrics.fMeasure());
+		
+		return null;
+		
+		
+		//lr, nb, dtree, rforest, gradientbTrees
+	}
 
 
 
